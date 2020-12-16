29239099177052f746561444efdfaaede7790745
ekaramyan <56231244+ekaramyan@users.noreply.github.com>
2020-12-16 22:51:43 +0300

    Добавьте файлы проекта.

---
+++ Rafctoring_Lab SoftDev/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rafctoring_Lab_SoftDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] source = { "-4", "-3", "-2", "-1", "0", "1", "2", "3", "4" };
+            string destination = null;
+            for (int i = 0; i < 3; i++)
+                destination += source[i];
+        }
+    }
+}
