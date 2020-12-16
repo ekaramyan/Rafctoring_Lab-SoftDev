4f1fbf3cc0822449595abd82995c4eda328453c0
ekaramyan <56231244+ekaramyan@users.noreply.github.com>
2020-12-17 00:37:42 +0300

    Чистка кода от мусора, оптимизация и рефакторинг

--- Rafctoring_Lab SoftDev/Program.cs
+++ Rafctoring_Lab SoftDev/Program.cs
@@ -11,27 +11,21 @@ namespace Rafctoring_Lab_SoftDev
             Console.WriteLine("Задание №1"); // Начало первого задания
 
             Console.WriteLine("Введите число с минимум одним числом после ЗАПЯТОЙ");
+            double double_x = Convert.ToDouble(Console.ReadLine());
+            int x = (int)((double_x - (int)double_x) * 10);
+            Console.WriteLine(x); // основная формула
 
-
-            Decimal varib = Convert.ToDecimal(Console.ReadLine());
-            int zn = (int)((varib - (int)varib) * 10); // основная формула
-
-
-            Console.WriteLine($"Искомое число : {zn}"); // Конец первого задания
+            Console.WriteLine($"Искомое число : {x}"); // Конец первого задания
 
 
 
 
             Console.WriteLine("\n\nЗадание №2");                                // Начало второго задания
 
-            int hours, minutes;
-
             Console.WriteLine("Сколько прошло секунд? : ");
-            int sec = Convert.ToInt32(Console.ReadLine());
-
-            hours = (sec / (60 * 60));
-            minutes = (sec - hours * 60 * 60) / 60;                                        //часы перевести в секунды и результат разделить на 60(для перевода из секунд в минуты)
-
+            int seconds = Convert.ToInt32(Console.ReadLine());
+            int hours = seconds / 3600;
+            int minutes = seconds / 60 - 60 * hours;                                    //часы перевести в секунды и результат разделить на 60(для перевода из секунд в минуты)
 
             Console.WriteLine($"Прошло {hours} часа (ов) , {minutes} минут(ы)");
             // Конец второго задания
@@ -45,54 +39,12 @@ namespace Rafctoring_Lab_SoftDev
             double corner;
             Console.WriteLine("Введите количество часов (желательно от 0 до 11) : ");
             int time_hours = Convert.ToInt32(Console.ReadLine());
-            if (time_hours <= 11)
-            {
-            }
-            else if (time_hours > 11)
-            {
-                Console.WriteLine("Слишком большое значение");
-                Environment.Exit(0);
-            }
-            else if (time_hours < 0)
-            {
-                Console.WriteLine("Слишком маленькое значение");
-                Environment.Exit(0);
-            }
-            else { Console.WriteLine("Буквы Нельзя!!!"); }
 
             Console.WriteLine("Введите количество минут (желательно от 0 до 59) : ");
             int time_minutes = Convert.ToInt32(Console.ReadLine());
-            if (time_minutes <= 59)
-            {
-            }
-            else if (time_minutes > 59)
-            {
-                Console.WriteLine("Слишком большое значение");
-                Environment.Exit(0);
-            }
-            else if (time_minutes < 0)
-            {
-                Console.WriteLine("Слишком маленькое значение");
-                Environment.Exit(0);
-            }
-            else { Console.WriteLine("Буквы Нельзя!!!"); }
 
             Console.WriteLine("Введите количество секунд (желательно от 0 до 59) : ");
             int time_seconds = Convert.ToInt32(Console.ReadLine());
-            if (time_seconds <= 59)
-            {
-            }
-            else if (time_seconds > 59)
-            {
-                Console.WriteLine("Слишком большое значение");
-                Environment.Exit(0);
-            }
-            else if (time_seconds < 0)
-            {
-                Console.WriteLine("Слишком маленькое значение");
-                Environment.Exit(0);
-            }
-            else { Console.WriteLine("Буквы Нельзя!!!"); }
 
             corner = (time_seconds + (time_minutes * 60) + (time_hours * 3600)) * 0.0083333;                                           // Расчет градусной меры угла, 0.008(333) количество градусов на секунду
 
@@ -111,19 +63,17 @@ namespace Rafctoring_Lab_SoftDev
             Console.WriteLine("\n\nЗадание №4");                                // Начало четвертого задания
 
             Console.WriteLine("Введите значение первой переменной : ");
-            int varib_1 = Convert.ToInt32(Console.ReadLine());
+            int var1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите значение второй переменной : ");
-            int varib_2 = Convert.ToInt32(Console.ReadLine());
+            int var2 = Convert.ToInt32(Console.ReadLine());
 
-
-            varib_2 = varib_1 + varib_2;                                    // Работа с переменными
-            varib_1 = varib_2 - varib_1;
-            varib_2 = varib_2 - varib_1;
-
+            var2 = var1 + var2;                                    // Работа с переменными
+            var1 = var2 - var1;
+            var2 = var2 - var1;
 
             Console.WriteLine($"Совершаем обмен значениями и получаем :" +
-                $" \nпервая переменная: {varib_1};" +
-                $" \nвторая переменная: {varib_2}");
+                $" \nпервая переменная: {var1};" +
+                $" \nвторая переменная: {var2}");
             // Конец четвертого задания 
 
 
@@ -154,13 +104,8 @@ namespace Rafctoring_Lab_SoftDev
             Console.WriteLine("Введите четырехзначное число: ");
             int num = Convert.ToInt32(Console.ReadLine());
 
-            int thousands = num / 1000;                                   // Формулы для нахождения тысяч, сотен, десятков и единиц в числе
-            int hundreds = num / 100 - thousands * 10;
-            int dozens = num / 10 - thousands * 100 - hundreds * 10;
-            int units = num - thousands * 1000 - hundreds * 100 - dozens * 10;
-
-            int compos = thousands * hundreds * dozens * units;
-            Console.WriteLine($"Произведение цифр в нем = {compos}");
+            int result = (num % 10 + (num % 100) / 10 + (num % 1000 - num % 100) / 100 + (num - num % 1000) / 1000);
+            Console.WriteLine($"Произведение цифр в нем = {result}");
             // Конец шестого задания
 
         }
